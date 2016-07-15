289d56e7f62d76eac6befe36cf5486b9a3055e2d
travis.h.oneill@gmail.com <travis.h.oneill@gmail.com>
2016-07-14 17:58:38 -0700

    version 0.0

--- game.js
+++ game.js
@@ -1,7 +1,9 @@
 const GameView = require('./lib/game_view');
+const ImageLibrary = require('./lib/image_library');
 
 let canvas;
 let canvasContext;
+let game = 'game';
 
 window.onload = function(){
   console.log("Hello World");
@@ -9,43 +11,18 @@ window.onload = function(){
   canvasContext = canvas.getContext('2d');
   canvasContext.fillStyle = 'black';
   canvasContext.fillRect(0, 0, canvas.width, canvas.height);
-  const game = new GameView(canvas);
-  game.start();
 
+  const library = new ImageLibrary();
 
-  // const FPS = 30;
-  // setInterval(function() {
-  //   moveEveything();
-  //   drawEveything();
-  // }, 1000 / FPS);
+  // game = new GameView(canvas, library.images);
+  // game.start();
 
-//   canvas.addEventListener('mousemove', function(evt){
-//     let mousePos = calcMousePos(evt).y
-//     paddle1y = mousePos;
-//   })
+  setInterval(function(){
+    if(!game.status){
+      clearInterval(game.interval);
+      game = new GameView(canvas, library.images);
+      game.start();
+    }
+  }, 50);
+
 };
-
-// function drawEveything(){
-//   console.log("called draw");
-//   colorRect(0, 0, canvas.width, canvas.height, 'black');
-//   colorRect(5, paddle1y - 75, 15, paddleHeight, "white");
-//   // colorRect(ballx, bally, ballWidth, ballHeight, 'red');
-//   canvasContext.fillStyle = 'red';
-//   canvasContext.beginPath();
-//   canvasContext.arc(ballx, bally, ballWidth/2, 0, Math.PI*2, true);
-//   canvasContext.fill();
-// }
-//
-// function moveEveything(){
-//   ballx += ballSpeedx;
-//   bally += ballSpeedy;
-//   // ballSpeedx += 1;
-//   // ballSpeedy += 1;
-//   if(ballx > canvas.width - 10 || ballx < 10){ballSpeedx = -ballSpeedx;}
-//   if(bally > canvas.height - 10 || bally < 10){ballSpeedy = -ballSpeedy;}
-// }
-//
-// function colorRect(leftx, topy, width, height, color){
-//   canvasContext.fillStyle = color;
-//   canvasContext.fillRect(leftx, topy, width, height, color);
-// }
